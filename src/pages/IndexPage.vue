fa4f0e58ed27aa4ba1ccbde1b71f3b10c6a895ba
Tatiana <tanya_13.88@mail.ru>
2023-06-02 14:10:33 +0300

    buttons added

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,10 +1,31 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page class="flex flex-center q-gutter-lg q-pa-lg">
+
+      <q-btn
+        class="bubbleBtns"
+        glossy
+        color="green"
+        label="Курсор"
+      />
+      <q-btn
+        class="bubbleBtns"
+        glossy
+        color="green"
+        label="Смещение"
+      />
+      <q-btn
+        class="bubbleBtns"
+        glossy
+        color="green"
+        label="Вращение"
+      />
+      <q-btn
+        class="bubbleBtns"
+        color="green"
+        glossy
+        label="Масштабирование"
+      />
+      <canvas id="renderCanvas" touch-action="none"></canvas>
   </q-page>
 </template>
 
@@ -12,6 +33,16 @@
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'IndexPage'
+  name: 'IndexPage',
+  mounted() {
+    const canvas = document.querySelector("canvas")
+  }
 })
 </script>
+<style>
+  #renderCanvas {
+        width: 100%;
+        height: 100%;
+        touch-action: none;
+      }
+</style>
